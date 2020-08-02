2569d76b53f39009d3d1596230d12f172ff77ed3
cafetamine <arek.bystrowicz@gmail.com>
2020-08-02 18:00:03 +0200

    add spring context test

---
+++ lds-service/src/test/java/com/actilive/lds/LdsApplicationTest.java
@@ -0,0 +1,13 @@
+package com.actilive.lds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdsApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

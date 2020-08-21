2ef629fb3fb4dc4a22ba0e9cae8f3d8ddd5f0cc1
cafetamine <arek.bystrowicz@gmail.com>
2020-08-21 19:38:32 +0200

    add db table for addresses

--- lds-service/src/main/resources/data.sql
+++ lds-service/src/main/resources/data.sql
@@ -2,3 +2,8 @@ insert into LOCATION (Latitude, Longitude, Altitude)
      values ('10.123456', '10.123456', '10.123456'),
             ('11.123456', '11.123456', '11.123456'),
             ('12.123456', '12.123456', '12.123456');
+
+insert into ADDRESS (Country, State, City, Street, Number, PostalCode)
+     values ('Country1', 'State1', 'City1', 'Street1', 'Number1', 'PostalCode1'),
+            ('Country2', 'State2', 'City2', 'Street2', 'Number2', 'PostalCode2'),
+            ('Country3', 'State3', 'City3', 'Street3', 'Number3', 'PostalCode3');
--- lds-service/src/main/resources/schema.sql
+++ lds-service/src/main/resources/schema.sql
@@ -1,8 +1,19 @@
 drop table if exists LOCATION;
+drop table if exists ADDRESS;
 
 create table LOCATION(
-  Id bigint auto_increment primary key ,
+  Id bigint auto_increment primary key,
   Latitude varchar(25) not null,
   Longitude varchar(25) not null,
   Altitude varchar(25)
+);
+
+create table ADDRESS(
+  Id bigint auto_increment primary key,
+  Country varchar(120) not null,
+  State varchar(255) not null,
+  City varchar(255) not null,
+  Street varchar(255) not null,
+  Number varchar(25) not null,
+  PostalCode varchar(25) not null
 );

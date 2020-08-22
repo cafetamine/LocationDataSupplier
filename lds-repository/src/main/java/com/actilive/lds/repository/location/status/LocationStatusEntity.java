9f410779c4e614a7796d5405ba5aafe5aa7948a2
cafetamine <arek.bystrowicz@gmail.com>
2020-08-22 02:03:03 +0200

    add temporary zone fixes

--- lds-repository/src/main/java/com/actilive/lds/repository/location/status/LocationStatusEntity.java
+++ lds-repository/src/main/java/com/actilive/lds/repository/location/status/LocationStatusEntity.java
@@ -1,14 +1,13 @@
 package com.actilive.lds.repository.location.status;
 
-import com.actilive.lds.core.domain.location.coordinates.LocationCoordinates;
 import com.actilive.lds.core.domain.location.status.LocationStatus;
 import com.actilive.lds.core.domain.location.status.LocationStatusRead;
-import com.actilive.lds.repository.location.coordinates.LocationCoordinatesEntity;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
@@ -20,6 +19,7 @@ import java.time.ZoneOffset;
 @Table("LOCATION_STATUS")
 public class LocationStatusEntity {
 
+    @Id
     Long id;
     LocationStatusRead status;
     LocalDateTime lastReadDateTime;
@@ -27,14 +27,17 @@ public class LocationStatusEntity {
 
     // TODO handle time zones
     LocationStatus ToDomain() {
-        return new LocationStatus(id, status, lastReadDateTime.atOffset(ZoneOffset.UTC), nextReadDatetime.atOffset(ZoneOffset.UTC));
+        return new LocationStatus(id,
+                                  status,
+                                  lastReadDateTime != null ? lastReadDateTime.atOffset(ZoneOffset.UTC) : null,
+                                  nextReadDatetime.atOffset(ZoneOffset.UTC));
     }
 
     // TODO handle time zones
     static LocationStatusEntity FromDomain(@NotNull final LocationStatus status) {
         return new LocationStatusEntity(status.getId(),
                                         status.getStatus(),
-                                        status.getLastReadDateTime().toLocalDateTime(),
+                                        status.getLastReadDateTime() != null ? status.getLastReadDateTime().toLocalDateTime() : null,
                                         status.getNextReadDatetime().toLocalDateTime());
     }
 
